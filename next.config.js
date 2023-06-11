30fe1c3716efb0948dcba598511f98366df856d4
Rubical <zhpiv96@mail.ru>
2023-06-11 17:41:35 +0300

    upd

--- next.config.js
+++ next.config.js
@@ -4,5 +4,4 @@ module.exports = {
     SUPABASE_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkY2JyYnRmaHlrd2Zxc3V1a21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3Njc0NTksImV4cCI6MTk5NjM0MzQ1OX0.hLCJSbHWwKz8qAMBSC7qz3bnaZ1mg5Bd09hph17HhV8",
   },
-  output: 'export',
 };
